245b5727f12cddab01287b61c46d5370fdf10081
ipti004 <ipti004@ipti004-desktop>
2014-06-09 17:57:23 -0300

    feito terceiro form

--- src/java/com/guigoh/mandril/bean/SubmitFormBean.java
+++ src/java/com/guigoh/mandril/bean/SubmitFormBean.java
@@ -4,14 +4,17 @@
  */
 package com.guigoh.mandril.bean;
 
+import com.guigoh.mandril.entity.Author;
 import com.guigoh.mandril.entity.EducationalObject;
 import com.guigoh.primata.bo.InterestsBO;
 import com.guigoh.primata.entity.Interests;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -19,17 +22,35 @@ import javax.faces.bean.SessionScoped;
  */
 @SessionScoped
 @ManagedBean(name = "submitFormBean")
-public class SubmitFormBean implements Serializable{
-    
+public class SubmitFormBean implements Serializable {
+
     private EducationalObject educationalObject;
     private List<Interests> interestThemesList;
+    private List<Author> authorList;
+    private Author author;
     private String tags;
-    
-    public void init(){
-        interestThemesList = new ArrayList<Interests>();
-        loadInterestThemes();
+
+    public void init() {
+        if (!FacesContext.getCurrentInstance().isPostback()) {
+            interestThemesList = new ArrayList<Interests>();
+            authorList = new ArrayList<Author>();
+            author = new Author();
+            loadInterestThemes();
+        }
     }
-    
+
+    public void addAuthor() {
+        if (authorList.size() < 4) {
+            if (author.getName().matches("[a-zA-Z ]{3,40}")
+                    && author.getEmail().matches("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b")
+                    && author.getPhone().matches("\\(\\d{2}\\) \\d{4}-\\d{4}")
+                    && author.getSite().matches("(@)?(href=')?(HREF=')?(HREF=\")?(href=\")?(http://)?[a-zA-Z_0-9\\-]+(\\.\\w[a-zA-Z_0-9\\-]+)+(/[#&\\n\\-=?\\+\\%/\\.\\w]+)?")) {
+                authorList.add(author);
+                author = new Author();
+            }
+        }
+    }
+
     private void loadInterestThemes() {
         InterestsBO interestsBO = new InterestsBO();
         interestThemesList = interestsBO.findInterestsByInterestsTypeName("Themes");
@@ -58,5 +79,20 @@ public class SubmitFormBean implements Serializable{
     public void setTags(String tags) {
         this.tags = tags;
     }
-    
+
+    public List<Author> getAuthorList() {
+        return authorList;
+    }
+
+    public void setAuthorList(List<Author> authorList) {
+        this.authorList = authorList;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
--- web/resources/js/submitForm.js
+++ web/resources/js/submitForm.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var width = $(".progress").width();
     var page = 1;
+    $(".educational_object_author_phone").mask("(99) 9999-9999");
     $(document).on('click', '.button_back', function(){
         $(".form"+page+"_body").hide();
         $(".page-number").text(--page);
@@ -29,6 +30,12 @@ $(document).ready(function() {
                 $(".educational_object_tags_warning").css("display","block");
             }
         }
+        if(page == 3){
+            if ($(".author_added").length == 0){
+                validate = false;
+                $(".educational_object_author_warning2").css("display","block");
+            }
+        }
         //End
         if (validate == true){
             $(".form"+page+"_body").hide();
@@ -45,9 +52,27 @@ $(document).ready(function() {
             $(".educational_object_name_warning").hide();
             $(".educational_object_theme_warning").hide();
             $(".educational_object_tags_warning").hide();
+            $(".educational_object_author_warning1").hide();
+            $(".educational_object_author_warning2").hide();
         }
-        
-        
+       
+    });
+    $(document).on('click', '.button_add', function(){
+        if (!String($(".educational_object_author_name").val()).match("[a-zA-Z ]{3,40}")){
+            $(".educational_object_author_warning1").text("Digite um campo 'Nome' válido");
+            $(".educational_object_author_warning1").css("display","block");
+        }else if (!String($(".educational_object_author_email").val()).match("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b")){
+            $(".educational_object_author_warning1").text("Digite um campo 'E-mail' válido");
+            $(".educational_object_author_warning1").css("display","block");
+        }else if (!String($(".educational_object_author_phone").val()).match("\\(\\d{2}\\) \\d{4}-\\d{4}")){
+            $(".educational_object_author_warning1").text("Digite um campo 'Phone' válido");
+            $(".educational_object_author_warning1").css("display","block");
+        }else if (!String($(".educational_object_author_site").val()).match("(@)?(href=')?(HREF=')?(HREF=\")?(href=\")?(http://)?[a-zA-Z_0-9\\-]+(\\.\\w[a-zA-Z_0-9\\-]+)+(/[#&\\n\\-=?\\+\\%/\\.\\w]+)?")){
+            $(".educational_object_author_warning1").text("Digite um campo 'Site' válido");
+            $(".educational_object_author_warning1").css("display","block");
+        }else{
+            $(".educational_object_author_warning1").hide();
+        }
     });
 });
 
