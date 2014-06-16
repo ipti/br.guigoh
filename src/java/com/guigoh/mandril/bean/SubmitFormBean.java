58059ccd4c6b952279b7d8b43275f479be74091a
ipti004 <ipti004@ipti004-desktop>
2014-06-16 13:03:37 -0300

    terminado o front-end do form

--- src/java/com/guigoh/mandril/bean/SubmitFormBean.java
+++ src/java/com/guigoh/mandril/bean/SubmitFormBean.java
@@ -12,10 +12,17 @@ import com.guigoh.primata.bo.SocialProfileBO;
 import com.guigoh.primata.bo.util.CookieService;
 import com.guigoh.primata.entity.Interests;
 import com.guigoh.primata.entity.SocialProfile;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
@@ -68,23 +75,75 @@ public class SubmitFormBean implements Serializable {
 
     public void addMedia() throws IOException {
         if (mediaList.size() < 3) {
-            if(!mediaFile.getSubmittedFileName().equals("")){
+            if (!mediaFile.getSubmittedFileName().equals("")) {
                 mediaList.add(mediaFile);
             }
         }
     }
 
-    
-    public void submitForm() {
-        System.out.println(educationalObject.getName());
+    public void submitForm() throws IOException {
         EducationalObjectBO educationalObjectBO = new EducationalObjectBO();
         SocialProfileBO socialProfileBO = new SocialProfileBO();
-        educationalObject.setSocialProfileId(socialProfileBO.findSocialProfile(CookieService.getCookie("token")));
+        SocialProfile socialProfile = socialProfileBO.findSocialProfile(CookieService.getCookie("token"));
+        educationalObject.setSocialProfileId(socialProfile);
         educationalObject.setStatus("PE");
         educationalObject.setDate(educationalObjectBO.getServerTime());
-        educationalObject.setImage("/teste");
+        String path = "/home/ipti004/Imagens/"+educationalObject.getName()+"/image/";
+        uploadFile(imageFile, path);
+        educationalObject.setImage("http://cdn.guigoh.com/mandrildata/educationalobjects/" + socialProfile.getSocialProfileId() + "." + imageFile.getContentType().split("/")[1]);
         educationalObjectBO.create(educationalObject);
         submitted = true;
+    }
+    
+    private boolean uploadFile(Part part, String path) throws IOException {
+ 
+        boolean success;
+        // Extract file name from content-disposition header of file part
+        String fileName = getFileName(part);
+        System.out.println("***** fileName: " + fileName);
+ 
+        String basePath = path;
+        System.out.println(basePath);
+        File outputFilePath = new File(basePath + fileName);
+ 
+        // Copy uploaded file to destination path
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = part.getInputStream();
+            outputStream = new FileOutputStream(outputFilePath);
+ 
+            int read = 0;
+            final byte[] bytes = new byte[1024];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+ 
+            success = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            success = false;
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return success;
+    }
+    
+    private String getFileName(Part part) {
+        final String partHeader = part.getHeader("content-disposition");
+        System.out.println("***** partHeader: " + partHeader);
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(content.indexOf('=') + 1).trim()
+                        .replace("\"", "");
+            }
+        }
+        return null;
     }
 
     private void loadInterestThemes() {
