efdedc0f17d8695175f53c6f4800d284382c08a3
ipti008 <ipti008@ipti008-desktop>
2014-06-16 10:28:08 -0300

    migração do mandril

--- web/resources/js/home.js
+++ web/resources/js/home.js
@@ -2,9 +2,16 @@ $(document).ready(function(){
     var left = $(".left_column").height();
     var middle = $(".middle_column").height();
     var right = $(".right_column").height();
+    var educationalObjectName = $(".educational_object_name");
     if(right > middle){
         $(".middle_column").css("height", right);
     }else if (left > middle){
         $(".middle_column").css("height", left);
     }
+    $.each(educationalObjectName, function(){
+        var nameTemp = $(this).text();
+        if (nameTemp.length > 28){
+            $(this).text(nameTemp.substring(0, 28)+"...");
+        }
+    });
 });
