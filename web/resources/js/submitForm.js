d1156bcb3bf20315a3ccfe475aa140973834f61b
Francisco Cabral <franciscocabralfilho@gmail.com>
2014-11-06 14:44:35 -0200

    tratamento de nomes muito grandes de arquivos; mudança de upload para apenas 1 mídia; correção da barra de progresso voltando a 0

--- web/resources/js/submitForm.js
+++ web/resources/js/submitForm.js
@@ -105,8 +105,12 @@ $(document).ready(function() {
                 $(".educational_object_image_warning").hide();
             }
             reader.readAsDataURL(file);
+            if (file.name.length <= 32) {
+                $("#selectedImageName").text(file.name);
+            } else {
+                $("#selectedImageName").text(file.name.substring(0, 15) + "..." + file.name.substring(file.name.length - 15, file.name.length));
+            }
         }
-        $("#selectedImageName").text(file.name);
     });
 
     $(document).on('click', '.button_submit', function() {
@@ -143,83 +147,159 @@ $(document).ready(function() {
 
     $(document).on("change", "#mediaFile1", function() {
         media1 = this.files[0];
-        $("#selectedMediaName1").text(media1.name);
-        $("#uploaded_file1").text(media1.name);
+        if (media1.name.length <= 32) {
+            $("#selectedMediaName1").text(media1.name);
+            $("#uploaded_file1").text(media1.name);
+        } else {
+            $("#selectedMediaName1").text(media1.name.substring(0, 15) + "..." + media1.name.substring(media1.name.length - 15, media1.name.length));
+            $("#uploaded_file1").text(media1.name.substring(0, 15) + "..." + media1.name.substring(media1.name.length - 15, media1.name.length));
+        }
         $("#uploaded1").show();
         $(".educational_object_media_warning").css("display", "none");
     });
 
     $(document).on("change", "#mediaFile2", function() {
         media2 = this.files[0];
-        $("#selectedMediaName2").text(media2.name);
-        $("#uploaded_file2").text(media2.name);
+        if (media2.name.length <= 32) {
+            $("#selectedMediaName2").text(media2.name);
+            $("#uploaded_file2").text(media2.name);
+        } else {
+            $("#selectedMediaName2").text(media2.name.substring(0, 15) + "..." + media2.name.substring(media2.name.length - 15, media2.name.length));
+            $("#uploaded_file2").text(media2.name.substring(0, 15) + "..." + media2.name.substring(media2.name.length - 15, media2.name.length));
+        }
         $("#uploaded2").show();
         $(".educational_object_media_warning").css("display", "none");
     });
 
     $(document).on("change", "#mediaFile3", function() {
         media3 = this.files[0];
-        $("#selectedMediaName3").text(media3.name);
-        $("#uploaded_file3").text(media3.name);
+        if (media3.name.length <= 32) {
+            $("#selectedMediaName3").text(media3.name);
+            $("#uploaded_file3").text(media3.name);
+        } else {
+            $("#selectedMediaName3").text(media3.name.substring(0, 15) + "..." + media3.name.substring(media3.name.length - 15, media3.name.length));
+            $("#uploaded_file3").text(media3.name.substring(0, 15) + "..." + media3.name.substring(media3.name.length - 15, media3.name.length));
+        }
         $("#uploaded3").show();
         $(".educational_object_media_warning").css("display", "none");
     });
 
-    var submitted1 = true;
-    var submitted2 = true;
-    var submitted3 = true;
+//    função genérica pra seleção de arquivos
+//
+//    selectFile(this.file[0], 1);
+//    selectFile(this.file[1], 2);
+//    selectFile(this.file[2], 3);
+//    
+//    function selectFile(input, num){
+//        
+//        media = input;
+//        $("#selectedMediaName"+num).text(media.name);
+//        $("#uploaded_file"+num).text(media.name);
+//        $("#uploaded"+num).show();
+//        $(".educational_object_media_warning").css("display", "none");
+//    }
 
-    function finishUpload() {
-        if ((submitted1 == true) & (submitted2 == true) & (submitted3 == true)) {
-            $(".form5_body").hide();
-            $(".form6_body").show();
-        }
-    }
+
+//    método antigo (p/ 3 arquivos)
+//
+//    var submitted1 = true;
+//    var submitted2 = true;
+//    var submitted3 = true;
+//    
+//    function finishUpload() {
+//        if ((submitted1 == true) & (submitted2 == true) & (submitted3 == true)) {
+//            $(".form5_body").hide();
+//            $(".form6_body").show();
+//        }
+//    }
 
     $(document).on("click", "#submit", function() {
         if (typeof media1 != 'undefined') {
             var xhr1 = new XMLHttpRequest();
             if (xhr1.upload) {
                 var progress1;
+                var current1;
+                var difference1;
+                var loaded1;
                 xhr1.upload.onloadstart = function() {
-                    submitted1 = false;
+                    current1 = 0;
+                    difference1 = 0;
+                    loaded1 = 0;
+                    //submitted1 = false;
+                    $("#upload_bg1").css("width", "0%");
+                    $("#upload_percent1").text("0%");
                 }
                 xhr1.upload.onprogress = function(e) {
                     if (e.lengthComputable) {
-                        progress1 = Math.floor(e.loaded / e.total * 100);
-                        if (progress1 != 0) {
+                        var temp1 = current1;
+                        current1 = e.loaded;
+                        difference1 = current1 - temp1;
+                        if (difference1 <= 0) {
+                            difference1 = 0;
+                        }
+                        loaded1 += difference1;
+                        progress1 = Math.floor(loaded1 / e.total * 100);
+                        if (progress1 <= 100) {
                             $("#upload_bg1").css("width", progress1 + '%');
                             $("#upload_percent1").text(progress1 + '%');
+                        } else {
+                            finishUpload();
                         }
+                        //console.log("loaded: " + e.loaded + " / total: " + e.total + " / percentage: " + Math.floor(e.loaded / e.total * 100) + "%");
                     }
-                };
+                }
                 xhr1.upload.onloadend = function() {
-                    $("#upload_bg1").css("width", "100%");
-                    $("#upload_percent1").text("100%");
-                    $("#loading1").hide();
-                    $("#attached1").show();
-                    submitted1 = true;
                     finishUpload();
+//                    $("#upload_bg1").css("width", "100%");
+//                    $("#upload_percent1").text("100%");
+//                    $("#loading1").hide();
+//                    $("#attached1").show();
+//                    submitted1 = true;
+//                    finishUpload();      --------> finishUpload antigo (p/ 3 arquivos)
                 }
             }
             xhr1.open('post', "../mandril/submitForm.xhtml", true);
             xhr1.send(media1);
         }
 
+        function finishUpload() {
+            setTimeout(function() {
+                $("#loading1").hide();
+                $("#attached1").show();
+                $(".form5_body").hide();
+                $(".form6_body").show();
+            }, 2000);
+        }
+
         if (typeof media2 != 'undefined') {
             var xhr2 = new XMLHttpRequest();
             if (xhr2.upload) {
                 var progress2;
+                var current2;
+                var difference2;
+                var loaded2;
                 xhr2.upload.onloadstart = function() {
-                    submitted2 = false;
+                    current2 = 0;
+                    difference2 = 0;
+                    loaded2 = 0;
+                    //submitted2 = false;
+                    $("#upload_bg2").css("width", "0%");
+                    $("#upload_percent2").text("0%");
                 }
                 xhr2.upload.onprogress = function(e) {
                     if (e.lengthComputable) {
-                        progress2 = Math.floor(e.loaded / e.total * 100);
-                        if (progress2 != 0) {
-                            $("#upload_bg2").css("width", progress2 + '%');
-                            $("#upload_percent2").text(progress2 + '%');
+                        var temp2 = current2;
+                        current2 = e.loaded;
+                        difference2 = current2 - temp2;
+                        if (difference2 <= 0) {
+                            difference2 = 0;
                         }
+                        loaded2 += difference2;
+                        progress2 = Math.floor(loaded2 / e.total * 100);
+                        $("#upload_bg2").css("width", progress2 + '%');
+                        $("#upload_percent2").text(progress2 + '%');
+                        console.log("e2");
+                        console.log(e);
                     }
                 };
                 xhr2.upload.onloadend = function() {
@@ -227,7 +307,7 @@ $(document).ready(function() {
                     $("#upload_percent2").text("100%");
                     $("#loading2").hide();
                     $("#attached2").show();
-                    submitted2 = true;
+                    //submitted2 = true;
                     finishUpload();
                 }
             }
@@ -239,18 +319,31 @@ $(document).ready(function() {
             var xhr3 = new XMLHttpRequest();
             if (xhr3.upload) {
                 var progress3;
+                var current3;
+                var difference3;
+                var loaded3;
                 xhr3.upload.onloadstart = function() {
-                    submitted3 = false;
+                    current3 = 0;
+                    difference3 = 0;
+                    loaded3 = 0;
+                    //submitted3 = false;
                     $("#upload_bg3").css("width", "0%");
                     $("#upload_percent3").text("0%");
                 }
                 xhr3.upload.onprogress = function(e) {
                     if (e.lengthComputable) {
-                        progress3 = Math.floor(e.loaded / e.total * 100);
-                        if (progress3 != 0) {
-                            $("#upload_bg3").css("width", progress3 + '%');
-                            $("#upload_percent3").text(progress3 + '%');
+                        var temp3 = current3;
+                        current3 = e.loaded;
+                        difference3 = current3 - temp3;
+                        if (difference3 <= 0) {
+                            difference3 = 0;
                         }
+                        loaded3 += difference3;
+                        progress3 = Math.floor(loaded3 / e.total * 100);
+                        $("#upload_bg3").css("width", progress3 + '%');
+                        $("#upload_percent3").text(progress3 + '%');
+                        console.log("e3");
+                        console.log(e);
                     }
                 };
                 xhr3.upload.onloadend = function() {
@@ -258,7 +351,7 @@ $(document).ready(function() {
                     $("#upload_percent3").text("100%");
                     $("#loading3").hide();
                     $("#attached3").show();
-                    submitted3 = true;
+                    //submitted3 = true;
                     finishUpload();
                 }
             }
