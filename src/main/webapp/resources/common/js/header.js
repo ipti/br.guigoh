db4a2e984323f405f01a51b60c934f2868286cdf
iptipc008 <iptipc008@IPTIPC008.home>
2015-08-21 17:45:14 -0300

    resolvido: budega do box

--- src/main/webapp/resources/common/js/header.js
+++ src/main/webapp/resources/common/js/header.js
@@ -2,3 +2,11 @@ $(document).ready(function(){
     var locale = $("#localeAcronym").val();
     $("." + locale).css("font-weight", "bold");
 });
+
+$(document).on('click', function (e) {
+    if ($(e.target).closest($(".down-arrow")).length > 0){
+        $("#user-menu-box").toggle();
+    } else if ($(e.target).closest("#user-menu-box").length === 0) {
+        $("#user-menu-box").hide();
+    }
+});
