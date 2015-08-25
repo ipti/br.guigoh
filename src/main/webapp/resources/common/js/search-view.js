90688a0c458f5c343a43fbfd7388c0b03df9c492
Paulo <Paulo@DESKTOP-141O8H8.home>
2015-08-24 23:27:27 -0300

    fazendo tela de busca

--- src/main/webapp/resources/common/js/search-view.js
+++ src/main/webapp/resources/common/js/search-view.js
@@ -2,3 +2,8 @@ $(document).ready(function(){
     $(".menu-icon-five").parent().addClass("active");
 });
 
+$(".general-search").on('keypress', function (e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+        }
+    });
